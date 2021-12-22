68e03891b09bfdb94d1f73250131706e3cf6eaf7
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-12-22 09:09:22 +0100

    razor

--- SKYSWebDemo1/Pages/Index.cshtml.cs
+++ SKYSWebDemo1/Pages/Index.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using SKYSWebDemo1.Models;
@@ -18,8 +19,22 @@ namespace SKYSWebDemo1.Pages
             _logger = logger;
         }
 
+        public int Age { get; set; }
+
+        public string GreetingMessage { get; set; }
+
+        public List<string> Namn { get; set; } = new List<string>();
+
         public void OnGet()
         {
+
+            if (DateTime.Now.Hour > 17)
+                GreetingMessage = "Good night";
+            else
+                GreetingMessage = "Good morning";
+            Age = 12;
+            Namn.Add("Stefan");
+            Namn.Add("Oliver");
         }
     }
 }
